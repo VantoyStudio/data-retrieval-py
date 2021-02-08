91f1f8712ac47d757ec12404e4b12db8ba9620c3
asifhaider995 <asif.haider995@gmail.com>
2021-02-08 16:07:55 +0600

    payload compression

--- main.py
+++ main.py
@@ -8,29 +8,37 @@ import math
 import glob
 import pysftp
 
+### Tapo IP Cam credentials
+
 username = "asifhaider"
 password = "tapo332"
-ip = "192.168.43.121"
+ip = "192.168.0.103"
 port = "554"
 stream = "stream2"
 
-DURATION = 57
+### Collection Duration
+DURATION = 60
 
+### Remote PI ftp connection
+ip_remote = "103.129.37.38"
+username_remote = "pi"
+password_remote = "system"
+
+###########################################################
 
 def main():
     main_start = time.time()
     current_dt = dt.datetime.now()
-    src = "rtsp://"+username+":"+password+"/"+ip+":"+port+"/"+stream
+    src = "rtsp://"+username+":"+password+"@"+ip+":"+port+"/"+stream
 
     #############################################################################
-
-    cap = cv2.VideoCapture("http://dev.ddad-bd.com/storage/campaigns/2/videos/JwCsYLw7hXrfTpC6xZVvvzfaQmFVe99xZGUGj9rw.webm")
-    
+    print(src)
+    cap = cv2.VideoCapture(src)
     
     count = 0
 
     path = os.getcwd()
-    dataPath = path + "/data/" + current_dt.strftime("%Y-%m-%d-%X")
+    dataPath = path + "/data/" + current_dt.strftime("%Y-%m-%d-%H-%M-%S")
     os.mkdir(dataPath)
 
     start = time.time()
@@ -38,11 +46,15 @@ def main():
         current = time.time()
         ret, frame = cap.read()
         count += 1
-        filename = str(count)+".jpg"
+        filename = current_dt.strftime("%Y-%m-%d-%H-%M-%S")+str(count)+".jpg"
+        
+        
         if ret == True:
-            cv2.imshow("Test",frame)
-            # print(math.floor(current - start))
-            cv2.imwrite(dataPath+"/"+filename, frame)
+            # frame = cv2.resize(frame, (1024, 576))
+            if math.floor(current - start) % 3 == 0:
+                cv2.imwrite(dataPath+"/"+filename, frame)
+            
+            
             if math.floor(current - start) > DURATION:
                 break
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -50,15 +62,22 @@ def main():
         
         else:
             break
+
     cv2.destroyAllWindows()
     cap.release()
+    #############################################################################
+    import shutil
+
+    shutil.make_archive(dataPath, 'zip', dataPath)
+
 
     #############################################################################
+    with pysftp.Connection(ip_remote, username=username_remote, password=password_remote) as sftp:
+       with sftp.cd('dataPayload'):
+           sftp.put(remotepath="",localpath=dataPath+'.zip')
 
-    with pysftp.Connection('103.129.37.38', username="pi", password="system") as sftp:
-        with sftp.cd('dataPayload'):
-            sftp.put_r(remotepath="",localpath=dataPath)
-
+    #############################################################################
+    
     main_end = time.time()
     print(math.floor(main_end - main_start))
 if __name__ == "__main__":
